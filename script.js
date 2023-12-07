4e4800643c58db54ade2de6a20034924232aaf3e
soniachinwe <soniachinwe176@gmail.com>
2023-12-07 07:16:50 +0000

    js guess a number game

---
+++ script.js
@@ -0,0 +1,19 @@
+let btn = document.getElementById('btn');
+ 
+let display = document.getElementById('display');
+ 
+let gameNum = Math.floor(Math.random() * 100);
+
+btn.addEventListener('click' , function() {
+   let game = document.getElementById ('game').value;
+   if (game > gameNum) {
+    display.innerHTML = `Too high`
+   }else if (game < gameNum) {
+    display.innerHTML = `Too low`
+   }else if(game == gameNum) {
+    display.innerHTML = `Correct`
+   } else {
+    display.innerHTML = `error`
+   }
+   console.log(gameNum);
+})
